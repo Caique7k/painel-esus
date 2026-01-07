2acdd2f0132040b50317fe30872ade37da6b5927
Caique7k <caiquealves7k@gmail.com>
2026-01-06 22:37:25 -0300

    feat: add audio cleanup service with scheduled file removal

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CallModule } from './call/call.module';
 import { AudioModule } from './audio/audio.module';
 import { SectorModule } from './sector/sector.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [CallModule, AudioModule, SectorModule],
+  imports: [CallModule, AudioModule, SectorModule, ScheduleModule.forRoot()],
 })
 export class AppModule {}
